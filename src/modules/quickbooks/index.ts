5736e85dd0e9718ca78c59b0994d65fda49bb7e5
echorentals <patrick@echo-rentals.com>
2026-01-18 14:46:27 -0600

    feat: add QuickBooks module exports

---
+++ src/modules/quickbooks/index.ts
@@ -0,0 +1,17 @@
+export { getQuickBooksTokens, saveQuickBooksTokens } from './tokens.js';
+export type { QuickBooksTokens } from './tokens.js';
+export { getAuthUrl, handleAuthCallback, refreshTokenIfNeeded } from './auth.js';
+export {
+  getQuickBooksClient,
+  getBaseUrl,
+  createEstimate,
+  getEstimates,
+  getEstimate,
+  getCustomers,
+  findCustomerByName,
+  type QBEstimate,
+  type QBLineItem,
+  type QBCustomer,
+  type CreateEstimateInput,
+  type QuickBooksClient,
+} from './client.js';
